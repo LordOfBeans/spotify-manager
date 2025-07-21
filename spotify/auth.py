564dd2cabc5f6debf2dd88a5272d3a57c847b4d1
Sean Lord <sean13lord@gmail.com>
2025-07-21 11:44:47 -0400

    Updated Spotify API functions to take load off user

--- spotify/auth.py
+++ spotify/auth.py
@@ -1,10 +1,11 @@
 from urllib.parse import urlencode
-import webbrowser
-import socket
+import webbrowser # Opens application authorization window
+import socket # Handles callback
 from base64 import b64encode
 import requests
 import json
 from datetime import datetime, timedelta
+import os
 
 # Listens for callback on localhost port 8888
 def listenCallback(port):
@@ -41,9 +42,6 @@ def requestAuthCode(client_id, port):
 	webbrowser.open(auth_url)
 	return listenCallback(port)
 
-def getBase64AuthString(client_id, client_secret):
-	return str(b64encode(f'{client_id}:{client_secret}'.encode('utf-8')), 'utf-8')
-
 def requestAccessToken(b64_auth_string, auth_code, port):
 	headers = {
 		'Authorization': f'Basic {b64_auth_string}',
@@ -60,28 +58,72 @@ def requestAccessToken(b64_auth_string, auth_code, port):
 		raise Exception(f'Failed to get new access token with status code {resp.status_code}')
 	return json.loads(resp.text)
 
-def refreshAccessToken(b64_auth_string, refresh_token):
-	headers = {
-		'Authorization': f'Basic {b64_auth_string}',
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-	data = {
-		'grant_type': 'refresh_token',
-		'refresh_token': refresh_token
-	}
+# Handles everything to do with authorization
+# Makes singular get and post requests
+# Not responsible for looping through pages and whatnot
+class Auth:
+    def __init__(client_id, client_secret, callback_port, token_path):
+        self.client_id = client_id
+        self.b64_auth_string = str(b64encode(f'{client_id}:{client_secret}'.encode('utf-8')), 'utf-8')
+        self.callback_port = callback_port
+        self.token_path = token_path
 
-	resp = requests.post('https://accounts.spotify.com/api/token', data=data, headers=headers)
-	if resp.status_code != 200:
-		raise Exception(f'Failed to refresh access token with status code {resp.status_code}')
-	return json.loads(resp.text)
+        if not os.path.exists(token_path): # Get fresh access token and save
+            auth_code = requestAuthCode(self.client_id, self.callback_port)
+            self.token_info = requestAccessToken(self.b64_auth_string, auth_code, self.callback_port)
+            with open(self.token_path, 'w') as f:
+                json.dump(self.token_info, f)
+                self.expiration = datetime.now() + timedelta(seconds=self.token_info['expires_in'])
+        else: # Read access token from file
+            last_modified = os.path.getmtime(self.token_path)
+            with open(self.token_path, 'r') as f:
+                self.token_info = json.load(f)
+                self.expiration = datetime.fromtimestamp(last_modified) + timedelta(seconds=self.token_info['expires_in'])
+        self.headers = {
+            'Authorization': 'Bearer ' + self.token_info['access_token']
+            'Content-Type': 'application/json'
+        }
+        if datetime.now() > self.expiration_date:
+            self.refreshToken()
 
-# Spotify will only give you a new refresh token if the current refresh token is expired or does not exist
-# If no refresh token is supplied, one must be present in token_info
-def simplifyTokenInfo(token_info, refresh_token=None):
-	exp_seconds = token_info['expires_in']
-	exp_datetime = datetime.now() + timedelta(seconds=exp_seconds)
-	return {
-		'access_token': token_info['access_token'],
-		'expires_datetime': exp_datetime.strftime('%Y-%m-%d %H:%M:%S'),
-		'refresh_token': token_info['refresh_token']
-	}
+    # TODO: Have this return a value?
+    def refreshToken(self):
+        headers = {
+		    'Authorization': f'Basic {self.b64_auth_string}',
+		    'Content-Type': 'application/x-www-form-urlencoded'
+	    }
+	    data = {
+		    'grant_type': 'refresh_token',
+		    'refresh_token': self.token_info['refresh_token']
+	    }
+
+	    resp = requests.post('https://accounts.spotify.com/api/token', data=data, headers=headers)
+	    if resp.status_code != 200:
+		    raise Exception(f'Failed to refresh access token with status code {resp.status_code}')
+
+        new_token = json.loads(resp.text)
+        if 'refresh_token' in new_token: # A new refresh token is not always included in the response
+            refresh_token = self.token_info['refresh_token']
+            new_token['refresh_token'] = refresh_token
+        self.expiration = datetime.now() + timedelta(seconds=new_token['expires_in'])
+        with open(self.token_path, 'w') as f:
+            json.dump(new_token, f)
+        self.token_info = new_token
+        self.headers['Authorization'] = 'Bearer ' + self.token_info['access_token']
+
+    # Wrapper function for ease-of-use
+    def getEndpoint(self, endpoint, params=None):
+        url = 'https://api.spotify.com/v1' + endpoint
+        return self.getUrl(url, params=params)
+
+    def getUrl(self, url, params=None):
+        resp = requests.get(url, params=params, headers=self.headers)
+        if resp.status_code == 200:
+            return(json.loads(resp.text))
+        if resp.status_code == 401:
+            self.refreshToken() # Gets new token and updates header
+            resp = requests.get(url, params=params, headers=self.headers) # Try making request with fresh header
+            if resp.status_code == 200:
+                return json.loads(resp.text)
+        # TODO: Change exception type to something more meaningful
+        raise Exception(f'Failed to get {resp.request.url} with status code {resp.status_code}')
